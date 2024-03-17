050296134b089efec104566c7b8849c753aed45a
1ARZAMAS <lizaakutkina@gmail.com>
2024-03-17 14:56:30 +0700

    Added triangle

--- shape.cpp
+++ shape.cpp
@@ -8,28 +8,71 @@ void isoscelesTrapezoid(double lowerBase, double upperBase, double height);   //
 void rectangularTrapezoid(double lowerBase, double upperBase, double straighSide);  //прямоугольная трапеция
 void anarbitraryTrapezoid(double lowerBase, double upperBase, double rightSide, double leftSide, double height);  //произвольная трапеция
 
+int triangle();
+int Perimetr(int a, int b, int c); // Ищем периметр треугольника
+double Geron(int a, int b, int c, int P); // Площадь по формуле Герона
+bool rav_bedr(int a, int b, int c); // проверка на равнобедренность
+
 int main(){
-    cout << " Choose a shape: " << endl;
-    cout << " 1 - triangle," << endl;
-    cout << " 2 - trapezoid," << endl;
-    cout << " 3 - circle" << endl;
+    cout << " Выберите фигуру: " << endl;
+    cout << " 1 - треугольник," << endl;
+    cout << " 2 - трапеция," << endl;
     char shape;
     cin >> shape;
     switch (shape){
         case ('1'):
-            cout << "Салям" << endl;
+            system("cls");
+            triangle();
             break;
         case ('2'): 
             system("cls");
             trapeziod();
-             break;
-        case ('3'): //для окружности
             break;
         default: 
             cout << " The shape is not selected " << "\n";
             break;
     }
     return 0;
+}
+
+int triangle(){
+  cout<< "Введите стороны треугольника:"<<endl;
+  int a,b,c;
+  cin >> a >> b >> c;
+  if (a +b <= c or a+c <= b or b+c <= a){ // проверка на существование треугольника (если сумма двух сторон <= третьей, то трег не сущ)
+    cout << "Ошибка! Такой треугольник не может существовать!" << endl;
+ return 0;
+  }
+  
+  int P = Perimetr(a, b, c);
+  cout << "Периметр равен: " << P << endl;
+  double S = Geron(a, b, c, P);
+  cout << "Площадь равна: " << S <<endl;
+  bool is_rb = rav_bedr(a, b, c);
+  if(is_rb == true){
+    cout << "Треугольник равнобедренный";
+  } else {
+  cout << "Треугольник неравнобедренный";
+}
+return 0;
+}
+
+int Perimetr(int a, int b, int c){ // Ищем периметр треугольника
+  int sum = a+b+c;
+  return sum;
+}
+
+double Geron(int a, int b, int c, int P){ // Площадь по формуле Герона
+  double S = sqrt(P/2*(P-a)*(P-b)*(P-c));
+  return S;
+}
+
+bool rav_bedr(int a, int b, int c){ // проверка на равнобедренность
+  if (a == b or a==c or b==c){
+    return true;
+  } else {
+  return false;
+}
 }
 
 int trapeziod() {
@@ -130,7 +173,6 @@ double isosceleSide(double lowerBase, double upperBase, double height) {
 double rectangularSide(double lowerBase, double upperBase, double height){
     return sqrt((upperBase - lowerBase) * (upperBase - lowerBase) + (height * height)); 
 }
-
 
 void isoscelesTrapezoid(double lowerBase, double upperBase, double height){
     double tr_middleLine = middleLine(lowerBase, upperBase);
